4e8699a607c642f5dea25b6fa93e6310b397b2b2
Meng Wang <wm19922009@gmail.com>
2015-04-15 23:16:46 -0400

    port

--- application.py
+++ application.py
@@ -15,7 +15,7 @@ alchemyapi = AlchemyAPI()
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
 
-# Handler 
+# Handler
 LOG_FILE = '/opt/python/log/sample-app.log'
 handler = logging.handlers.RotatingFileHandler(LOG_FILE, maxBytes=1048576, backupCount=5)
 handler.setLevel(logging.INFO)
@@ -52,5 +52,6 @@ def worker():
 
 	return response
 
+
 if __name__ == '__main__':
-	application.run(host='0.0.0.0')
+	application.run(host='0.0.0.0', port=80)
